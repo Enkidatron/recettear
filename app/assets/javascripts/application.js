c3694bfee55e7621d2827b7c5b235b6bdda4cc36
Ben S. Thomas <fearlessleaderthomas@gmail.com>
2015-01-20 02:43:28 +0000

    Install React, Browserify, Coffeeify, Reactify, update npm

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require react
-//= require components
 //= require react_ujs
-//= require_tree .
+//= require components
+// require_tree .
--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module Recettear
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.browserify_rails.commandline_options = "-t coffeeify --extension=\".js.coffee\""
+    config.browserify_rails.commandline_options = "-t coffeeify --extension=\".js.coffee\" -t reactify --extension=\".js.jsx\""
   end
 end
